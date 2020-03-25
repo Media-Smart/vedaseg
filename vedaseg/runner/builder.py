8c3e61e9ea4f1f07b8c93f925cad896a4535ca6e
tianhewang <tianhe.wang@media-smart.cn>
2020-03-25 14:22:27 +0800

    style: assembler related naming fix

--- vedaseg/runner/builder.py
+++ vedaseg/runner/builder.py
@@ -4,7 +4,5 @@ from .registry import RUNNERS
 
 
 def build_runner(cfg, default_args=None):
-    #import pdb
-    #pdb.set_trace()
     runner = build_from_cfg(cfg, RUNNERS, default_args)
     return runner
